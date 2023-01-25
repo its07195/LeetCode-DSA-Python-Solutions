4b134cbaf389dc11bd7e167a71d31797f8947691
its07195 <cloudiecloudstrife@gmail.com>
2023-01-25 23:08:20 +0530

    d25

---
+++ LeetCode/2359.py
@@ -0,0 +1,30 @@
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        n = len(edges)
+        
+        def getDist(start, n):
+            queue = [start]
+            d = [float('inf') for i in range(n)]
+            d[start] = 0
+            while queue:
+                node = queue.pop(0)
+                x = edges[node]
+                if x == -1:
+                    continue
+                if (d[node]+1 < d[x]):
+                    d[x] = d[node]+1
+                    queue.append(x)
+            return d
+        
+        d1 = getDist(node1, n)
+        d2 = getDist(node2, n)
+        ans = -1
+        index = -1
+        for i in range(n):
+            if d1[i] == float('inf') or d2[i] == float('inf'):
+                continue
+            dist = max(d1[i], d2[i])
+            if ans == -1 or dist < ans:
+                ans = dist
+                index = i
+        return index
